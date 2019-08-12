f82e296e0f42d603219d64e8bd157bb53975eeda
blagovest <blaje@abv.bg>
2019-08-12 17:43:24 +0300

    configure featuredRooms data

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -5,6 +5,10 @@ export default class FeaturedRooms extends Component {
     static contextType = RoomContext;
     render() {
 
+        const {featuredRooms : rooms} = this.context;
+
+        console.log(rooms)
+
         return (
             <div>
                 hello from featured rooms 
